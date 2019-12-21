f9b83be89d13327a9100d17e37f0c98c80677093
PaulZ2020 <pavel.z2020@tut.by>
2019-12-21 22:00:37 +0300

    Added team

--- js/main.js
+++ js/main.js
@@ -19,4 +19,13 @@ $(document).ready(function () {
                 $('.filter > div[filter=' + filter + ']').show(3000);
             }
     });
+
+    $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false,
+        dots: true,
+        dotsClass: 'dots-style'
+    });
 });
