0bcfc22c4304bb1713319ec2921fa410777d46b4
PaulZ2020 <pavel.z2020@tut.by>
2019-12-22 10:20:02 +0300

    done layout

--- js/main.js
+++ js/main.js
@@ -22,10 +22,36 @@ $(document).ready(function () {
 
     $('.multiple-items').slick({
         infinite: true,
+        speed: 300,
         slidesToShow: 3,
         slidesToScroll: 3,
         arrows: false,
         dots: true,
-        dotsClass: 'dots-style'
+        dotsClass: 'dots-style',
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 });
