0e64077894f2728631b7ee4ac439103aadf3dba1
PaulZ2020 <pavel.z2020@tut.by>
2019-12-23 12:12:04 +0300

    end

--- js/main.js
+++ js/main.js
@@ -33,9 +33,7 @@ $(document).ready(function () {
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    dots: true
+                    slidesToScroll: 3
                 }
             },
             {
@@ -54,4 +52,39 @@ $(document).ready(function () {
             }
         ]
     });
-});
+/*Меню*/
+    $('ul.menu a[href^="#"').click(function(){
+        var target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 500);
+        $('ul.menu a[href^="#"').css({'color': '#212121'});
+        $(this).css({'color': '#004bee'});
+        return false;
+    });
+/*Выпадающее меню*/
+    $('.menu-icon').click(function(){
+        $('nav').slideToggle(500);
+        $('ul.menu').css({
+            'display':'flex',
+            'flex-direction':'column'
+        })
+        if($('.menu-icon').html()=='<i class="fas fa-bars"></i>'){
+            $(this).html('<i class="fas fa-times"></i>')
+        } else {
+            $(this).html('<i class="fas fa-bars"></i>')
+        }
+    })
+
+    $(window).scroll(function(){
+        if($(this).scrollTop()!=0)
+        $('#toTop').fadeIn();
+        else
+        $('#toTop').fadeOut();
+    });
+    $('#toTop').click(function(){
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+    });
+})
